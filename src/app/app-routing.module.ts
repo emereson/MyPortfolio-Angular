57a00674fa0ec7714c1db807caa1859a33025a28
emerson <escanorrosa123@gmail.com>
2023-04-09 02:17:40 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BanerComponent } from './components/baner/baner.component';
+import { AboutmeComponent } from './components/aboutme/aboutme.component';
+import { MyProjectsComponent } from './components/my-projects/my-projects.component';
+import { SkillComponent } from './components/skill/skill.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home',
+    component:BanerComponent
+  },
+  {
+    path:'aboutme',
+    component:AboutmeComponent
+  },
+  {
+    path:'my-projects',
+    component:MyProjectsComponent
+  },
+  {
+    path:'skill',
+    component:SkillComponent
+  },
+  {
+    path:'**',
+    redirectTo:'/home',
+    pathMatch:'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { BanerComponent } from './components/baner/baner.component';
+import { AboutmeComponent } from './components/aboutme/aboutme.component';
+import { MyProjectsComponent } from './components/my-projects/my-projects.component';
+import { SkillComponent } from './components/skill/skill.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    BanerComponent,
+    AboutmeComponent,
+    MyProjectsComponent,
+    SkillComponent
   ],
   imports: [
     BrowserModule,
